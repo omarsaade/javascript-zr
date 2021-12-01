d2cde7fd730fb2a3920e2de72d19f06240415c93
omar saade <omarsaadeh11@gmail.com>
2021-12-01 23:07:26 +0200

    javascript test

---
+++ learn.js
@@ -0,0 +1,72 @@
+/* window.onload = function() {
+ document.querySelector("h1").style.color = "red";}
+ wait the window to load 
+ btkhalik tektob w ta3mul reload  elo lama ykun demen <head></head> 
+ li2an iza ma ken fi window.onload = function() {} w hatayna script bel
+ head w fia esem el file js w bel body fi el html malah ytabe2 el ossa,
+ so ya nhet he el tari2a aw nhet el script ekher el body w nrayeh rasna */
+
+//  window.alert("hello");
+//  document.write("<h1>gelloooo</h1>");
+//  console.log("hello");
+//  console.error("oh");
+//  console.table(["ahmad","omar","ali"]);
+//  console.log("hello form %cjs %cfile","color:red;font-size : 40px","color:blue;font-size : 40px")
+//  //  array => object
+// console.log("omar saade");
+// console.log(typeof "omar saade");
+// console.log(typeof 5000.99);
+// console.log(typeof [10,15,17]);
+// console.log(typeof ["ali","ahmad"]);
+// console.log(typeof {name:'osama',age : 14 , country:"eg"}); Object
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+//  var user = "omar saade"; declare
+
+//  console.log(user); now u can use it
+
+//  <div id="hello">wawww</div> lezem het he bel body
+//  hello.innerHTML = "option"; he bel .js file bta5line gayer mu7atawa el div..kha
+//  khalas saret el hello ka2ana variable
+// let a  = 1 ; // bta3ti error w bt2elak iza fi galat w hye fix la kell shaglet bta3arfa abla
+// console.log('el zero web "school"');
+
+
+// console.log("el zero 'webschool'")
+// console.log("el zero web \"school\"")
+// console.log('el zero web \"school\"')
+// console.log('el zero \\ web \"school\"')
+// console.log("el zero\
+// web\
+// school");
+// console.log("elzero\nweb\nSchool");
+
+// let a = "we love";
+// let b = "javascript";
+// // document.write(a + b);
+// document.write(a + " " + b);// bta3mul space
+// console.log(a,b);// bta3mul space la 7ala ben el a and b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
