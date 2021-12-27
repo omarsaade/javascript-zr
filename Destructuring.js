77d7630789c1750658e74bb68dccc1732ecfe5d1
omar saade <omarsaadeh11@gmail.com>
2021-12-27 18:32:03 +0200

    array

--- Destructuring.js
+++ Destructuring.js
@@ -716,6 +716,14 @@
 
 // console.log({ ...objOne, ...objTwo, e: 5 });
 
+// let n1 = [10, 30, 10, 20];
+// let n2 = [30, 20, 10];
+
+// console.log(Math.max(...n1) * [...n1, ...n2].length) // 210
+
+
+
+
 
 
 
